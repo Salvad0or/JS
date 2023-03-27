273c94d451f4dab18acc25b77ce7e06e8a840b23
- <wavevape@gmail.com>
2023-03-27 20:21:55 +0300

    first commit

---
+++ HelloWordl.js
@@ -0,0 +1,32 @@
+
+console.log('Показать что-то в консоли');
+
+// переменные
+//#region 
+let flag = true;
+
+let x = 7;
+let y = .7;
+let z = 0.8;
+let u = 8.;
+
+let str = `hi`;
+let str1 = "hi\n";
+let str2 = 'hi';
+
+//#endregion
+
+// массивы
+//#region 
+
+let mass = ['aa','sdf'];
+
+let empty = [];
+
+empty.unshift('Добавить в начало массива')
+empty.push('Добавить в конец массива')
+
+let y = empty.length;
+
+
+//#endregion
